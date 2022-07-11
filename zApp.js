0f136e5f4a8cd5c69512df6d51ba817c3caedf4c
Severo <cmsdof@gmail.com>
2022-07-11 20:47:22 -0300

    Set-up para Novo Projeto + JS Side Project
    Mudança para Netbeans + Criado arquivo Javascript.

---
+++ zApp.js
@@ -0,0 +1,12 @@
+let x;
+
+console.log("Lista de Exercícios");
+console.log("Escolha uma questão de 1 a 35:");
+
+switch (x) {
+    case 1:
+        console.log("Caso 1");
+        break;
+    default:
+        console.log(":(")
+}
